e67cb628f1912656b4b4fcfc3ff70c9d9e021da2
donnate <cdonnat@uchicago.edu>
2023-05-10 17:12:19 -0500

    fixing bash

--- experiments/sparse_CCA/bash_experiment.sh
+++ experiments/sparse_CCA/bash_experiment.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=array
 #SBATCH --output=experiments/sparse_CCA/logs/array_%A_%a.out
 #SBATCH --error=experiments/sparse_CCA/logs/array_%A_%a.err
-#SBATCH --array=1-10
+#SBATCH --array=1-50
 #SBATCH --time=35:00:00
 #SBATCH --partition=caslake
 #SBATCH --ntasks=1
