d2498c09b76fbcd4198983a152bf212e8921642c
donnate <cdonnat@uchicago.edu>
2023-05-14 13:50:38 -0500

    changing result file name bash_loc

--- experiments/sparse_CCA/bash_localized_experiment.sh
+++ experiments/sparse_CCA/bash_localized_experiment.sh
@@ -18,7 +18,7 @@ job_id=$SLURM_ARRAY_JOB_ID
 module load libgmp
 module load R/4.2.0
 
-result_file="${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}"
+result_file="localized_${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}"
 echo "result file is ${result_file}"
 cd $SCRATCH/$USER/group-CCA/
 Rscript experiments/sparse_CCA/experiment_localized_CCA.R $SLURM_ARRAY_TASK_ID $result_file $1 PA
