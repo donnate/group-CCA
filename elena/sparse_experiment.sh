4c4284c383678b4a0b1625d3bf525835419ab30d
donnate <cdonnat@uchicago.edu>
2023-10-28 12:14:54 -0500

    fix sh

--- elena/sparse_experiment.sh
+++ elena/sparse_experiment.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=array
-#SBATCH --output=experiments/sparse_CCA/logs/array_%A_%a.out
-#SBATCH --error=experiments/sparse_CCA/logs/array_%A_%a.err
+#SBATCH --output=elena/logs/array_%A_%a.out
+#SBATCH --error=elena/logs/array_%A_%a.err
 #SBATCH --array=1-2
 #SBATCH --time=6:00:00
 #SBATCH --partition=caslake
@@ -21,4 +21,4 @@ module load R/4.2.0
 result_file="${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}"
 echo "result file is ${result_file}"
 cd $SCRATCH/$USER/group-CCA/
-Rscript elena/missing/1.a.sparse_simulations.R $SLURM_ARRAY_TASK_ID $result_file
+Rscript elena/missing/1.a.sparse_simulations.R $SLURM_ARRAY_TASK_ID $result_file $1
