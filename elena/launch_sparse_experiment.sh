cec1b79a26c85a330cb832d5b7231e3e109cdf52
Claire Donnat <cdonnat@midway3-login4.rcc.local>
2023-11-20 21:39:36 -0600

    fix merge

--- elena/launch_sparse_experiment.sh
+++ elena/launch_sparse_experiment.sh
@@ -2,14 +2,17 @@
 
 # Define the values for the variables
 theta_strengths="high medium low"
-n_values="100 200 500 1000 2000 10000"
-p_values="20 80 500 1000"
+n_values="100 200 500 1000 2000"
+p_values="20 80 500 800 1000"
+r_values="1 3 5 10"
 
 for theta in $theta_strengths; do
   for n in $n_values; do
     for p in $p_values; do
-      sbatch elena/sparse_experiment.sh "$n" "$theta" "$p"
+      for r in $r_values; do
+      sbatch elena/sparse_experiment.sh "$n" "$theta" "$p" "$r
     #echo "$theta"
+      done
     done
   done
 done
